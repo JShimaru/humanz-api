f833050948e889a26111f876d97c9df3e6f9c8b4
jshimaru <55031303+JShimaru@users.noreply.github.com>
2022-08-06 21:21:10 -0400

    Initial publishing

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/favoriteGorillaz'
+const LOCAL_URI = 'mongodb://127.0.0.1:27017/favoriteGorillaz'
+const MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || LOCAL_URI
 
 mongoose.connect(MONGODB_URI, {useUnifiedTopology:true , useNewUrlParser:true }).then(()=>{
     console.log('Successfully connected to MongoDB!')
